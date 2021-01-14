32276c20012908663a6c6ef59b7d8cd52106a800
Muhammad Hifzhan Silmi <56281943+Akhdan26@users.noreply.github.com>
2021-01-14 15:22:49 +0700

    baru

---
+++ scripts/auth.js
@@ -0,0 +1,19 @@
+// login
+const loginForm = document.querySelector('.login-form');
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // get user info
+  const email = loginForm['login-email'].value;
+  const password = loginForm['login-password'].value;
+
+  // log the user in
+  auth.signInWithEmailAndPassword(email, password).then(() => {
+    window.location.href = "admin.html";
+  }).catch(() => {
+    alert("email atau password salah!");
+  }).finally(() => {
+    loginForm.reset();
+  });
+
+});
