359e02f813eaf694a9953e43c0997ec81d871284
hisyamzayd <hisyamzayd@gmail.com>
2021-01-18 15:10:51 +0700

    edit upload photo code

--- scripts/db_gb.js
+++ scripts/db_gb.js
@@ -8,33 +8,46 @@ guestForm.addEventListener("submit", (e) => {
   let suhu = guestForm["suhu"].value;
   let nama_peg = guestForm["nama_peg"].value;
   let keperluan = guestForm["keperluan"].value;
-  var foto_ktp = guestForm["foto_ktp"].value.split("\\"); 
-  foto_ktp = foto_ktp[foto_ktp.length - 1];
-  
+
   let p1 = guestForm["p1"].value;
   let p2 = guestForm["p2"].value;
   let p3 = guestForm["p3"].value;
   let p4 = guestForm["p4"].value;
   let p5 = guestForm["p5"].value;
   let p6 = guestForm["p6"].value;
- 
+
+  let foto_ktp_file = document.querySelector("#fotoktp").files[0];
+  let foto_ktp = new Date().toString();
+  let metadata = {
+    contentType:foto_ktp_file.type,
+  };
+  const task = ref.child(foto_ktp).put(foto_ktp_file,metadata);
+
   if (confirm("Apakah data yang anda masukan sudah benar?")) {
-      db.collection('tamu').add({
-          nama: nama,
-          nama_prhs: nama_prhs,
-          no_hp: no_hp,
-          suhu: suhu,
-          nama_peg: nama_peg,
-          keperluan: keperluan,
-          foto_ktp: foto_ktp,
-          p1: p1,
-          p2: p2,
-          p3: p3,
-          p4: p4,
-          p5: p5,
-          p6: p6
-      }).then(() => {
-          alert("Data anda berhasil disimpan");
+    db.collection("tamu")
+      .add({
+        nama: nama,
+        nama_prhs: nama_prhs,
+        no_hp: no_hp,
+        suhu: suhu,
+        nama_peg: nama_peg,
+        keperluan: keperluan,
+        foto_ktp: foto_ktp,
+        p1: p1,
+        p2: p2,
+        p3: p3,
+        p4: p4,
+        p5: p5,
+        p6: p6,
       })
+      .then(() => {
+        task
+          .then((snapshot) => snapshot.ref.getDownloadURL())
+          .then((url) => {
+            alert("Data anda berhasil disimpan");
+            const image = document.querySelector("#image");
+            image.src = url;
+          });
+      });
   }
 });
